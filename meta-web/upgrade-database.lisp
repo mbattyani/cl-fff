8cfc375ffc1b4811022e5d9c05306362fd1c6950
mbattyani <marc@battyani.com>
2004-10-06 21:09:05 +0000

    added upgrade-database
    git-svn-id: https://www.fractalconcept.com/fractal@221 bc2e6456-d2ce-0310-8166-9ee7e5ed2fe7

---
+++ meta-web/upgrade-database.lisp
@@ -0,0 +1,30 @@
+(in-package "META-WEB")
+
+(defun update-class-project-3-to-4 (store pass)
+  (let* ((*package* (find-package "META-WEB")) (class (find-class 'project)))
+    (when (eq pass :modify-tables)
+      (meta-level::add-slot-to-class-table store class 'other-documents)
+      (meta-level::add-slot-to-class-table store class 'project-version)
+      (meta-level::add-slot-to-class-table store class 'version-date))
+    (when (eq pass :modify-data)
+      (clsql-sys:with-database (nil nil :pool *database-pool*)
+        (clsql-sys:do-query ((object-id) "SELECT id  FROM project" :types :auto)
+          (let ((object (meta-level:load-object object-id store)))
+            (when object
+              (let ((data-object (meta-level::load-object-data object)))
+                (slot-makunbound data-object 'other-documents)
+                (slot-makunbound data-object 'project-version)
+                (slot-makunbound data-object 'version-date)
+                (meta-level::initialize-unbound-slots object)
+                (meta-level::mark-object-as-modified object))))))))
+  (meta-level::save-modified-objects store))
+
+
+(defun convert-base-from-version-3-to-4 (store)
+  (dolist (class '(other-document))
+     (meta::create-class-table store (find-class class)))
+(update-class-project-3-to-4 store :modify-tables)
+
+
+(update-class-project-3-to-4 store :modify-data)
+)
