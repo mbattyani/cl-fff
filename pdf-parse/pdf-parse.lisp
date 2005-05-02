cdbeba51b5ec5ed60073ac4a68b903c3e9c6e53d
mbattyani <marc@battyani.com>
2005-05-02 18:49:38 +0000

    pdf-parse: version fixed and up-to-date
    git-svn-id: https://www.fractalconcept.com/fractal@318 bc2e6456-d2ce-0310-8166-9ee7e5ed2fe7

--- pdf-parse/pdf-parse.lisp
+++ pdf-parse/pdf-parse.lisp
@@ -1,5 +1,8 @@
 (in-package pdf)
 
+#+nil
+(pushnew :use-cl-yacc *features*)
+
 (eval-when (:compile-toplevel :load-toplevel :execute)
   (require "parsergen"))
 
@@ -20,8 +23,9 @@
     (format *pdf-stream* "~d ~d R" (obj-number obj)(gen-number obj))))
 
 (defconstant +white-char+ (concatenate 'string '(#\Space #\Newline #\Return #\Tab #\Null #\Page)))
-(defmacro white-char-p (c)
-  `(find c, +white-char+))
+
+(defun white-char-p (c)
+  (find c +white-char+))
 
 (defun eat-comment ()
   (loop for c = (read-char *pdf-read-stream* nil *pdf-read-stream*)
@@ -44,7 +48,7 @@
     (read-sequence stream *pdf-read-stream*)
     stream))
 
-#+nil(defun pdf-lex0 ()
+(defun pdf-lex0 ()
   (let ((string :eof))
     (labels ((trig-char (c tc token)
 	       (when (char= c tc)
@@ -82,8 +86,9 @@
     (let ((token (pdf-lex0)))
       (cond
 	((eq token :eof) nil)
-	((symbolp token) token)
-	((every #'digit-char-p token)(setf *lex-val*(parse-integer token)) :integer)
+	((symbolp token) (values token *lex-val*))
+	((every #'digit-char-p token)
+	 (setf *lex-val*(parse-integer token)) (values :integer (parse-integer token)))
 	((string= token "R") :R)
 	((string= token "n") :n)
 	((string= token "f") :f)
@@ -91,12 +96,13 @@
 	((string= token "endobj") :endobj)
 	((string= token "stream") :stream)
 	((string= token "endstream") :endstream)
+	((string= token "startxref") nil)
 	((string= token "true")  :true)
 	((string= token "false") :false)
 	((string= token "xref") :xref)
 	((string= token "trailer") :trailer)
 	((string= token "null") :null)
-	(t (setf *lex-val* token) (print token) :token)))))
+	(t (setf *lex-val* token) (values :token token))))))
 
 (defun parse-pdf (file)
   (with-open-file (s file :direction :input :external-format '(:default :eol-style :lf))
@@ -113,49 +119,130 @@
       (setf startxref (parse-integer trailer :start (+ startxref-pos 10) :junk-allowed t))
       (file-position *pdf-read-stream* startxref)
       (let* ((trailer (parse-trailer))
-	     (trailer-dict (fourth trailer))
-	     (xref-first (first trailer))
-	     (nb-xrefs (second trailer))
-	     (xrefs (third trailer))
+	     (xrefs (first trailer))
+	     (trailer-dict (second trailer))
 	     (sorted-pos ())
 	     (last-object (get-dict-value trailer-dict "/Size"))
 	     (*document* (make-instance 'document :empty t))
 	     (objects (make-array (1+ last-object) :fill-pointer (1+ last-object) :adjustable t
 				  :initial-element nil)))
-      (setf (objects *document*) objects)
-      (setf sorted-pos (sort (cons startxref (loop for (pos gen type) in xrefs
-						   when (eq type :n) collect pos)) #'<))
-      (loop repeat nb-xrefs
-	for object-number from xref-first
-	for (pos gen type) in xrefs
-	when (eq type :n) do (setf (aref objects object-number)
-				   (read-raw-object pos object-number gen sorted-pos)))
-      (let* ((cat-number (obj-number (get-dict-value trailer-dict "/Root")))
-	     (catalog (parse-raw-object (aref objects cat-number)))
-	     (pages-number (obj-number (get-dict-value (content catalog) "/Pages")))
-	     (pages (parse-raw-object (aref objects pages-number))))
-	(setf pages (change-class pages 'page-node))
-	(setf (pages pages) (get-dict-value (content pages) "/Kids"))
-	(change-dict-value (content pages) "/Count" #'(lambda () (length (pages pages))))
-	(setf (root-page *document*) pages)
-	(setf (last-object-number *document*) last-object)
-	(setf (catalog *document*) catalog))
-      *document*))))
+	(loop with prev-trailer = nil and prev-trailer-dict = trailer-dict
+	   for prev-xref = (get-dict-value prev-trailer-dict "/Prev")
+	   while prev-xref do
+	     (file-position *pdf-read-stream* prev-xref)
+	     (setf prev-trailer (parse-trailer))
+	     (when prev-trailer 
+	       (setf xrefs (append (first prev-trailer) xrefs)
+		     prev-trailer-dict (second prev-trailer))))
+
+	(setf (objects *document*) objects)
+	(setf sorted-pos (sort (list* startxref file-length (loop for (pos gen type) in xrefs
+							       when (eq type :n) collect pos)) #'<))
+	(loop with object-number = 0
+	   for (pos gen type) in xrefs
+	   do (case type
+		(:n (setf (aref objects object-number)
+			  (read-raw-object pos object-number gen sorted-pos))
+		    (incf object-number))
+		(:f (incf object-number))
+		(:start-table (setf object-number pos))))
+	(let* ((cat-number (obj-number (get-dict-value trailer-dict "/Root")))
+;	     (doc-info   (get-dict-value trailer-dict "/Info"))
+;	     (doc-info-number (when doc-info (obj-number doc-info)))
+	       (catalog (parse-raw-object (aref objects cat-number)))
+	       (pages-number (obj-number (get-dict-value (content catalog) "/Pages")))
+	       (pages (parse-raw-object (aref objects pages-number))))
+	  (setf pages (change-class pages 'page-node))
+	  (setf (pages pages) (get-dict-value (content pages) "/Kids"))
+	  (change-dict-value (content pages) "/Count" #'(lambda () (length (pages pages))))
+	  (setf (root-page *document*) pages)
+	  (setf (last-object-number *document*) last-object)
+	  (setf (catalog *document*) catalog))
+	*document*))))
 
 (defun parse-trailer ()
   (let ((*force-eof* nil))
-    (pdf-object-parser 'pdf-lex)))
+    #-use-cl-yacc (pdf-object-parser 'pdf-lex)
+    #+use-cl-yacc (yacc:parse-with-lexer 'pdf-lex *pdf-parser*)))
 
+#+use-cl-yacc
+(eval-when (:compile-toplevel :load-toplevel)
+(defun convert-parser-to-cl-yacc (productions)
+  (flet ((make-arg-list (n)
+	   (loop for i from 1 to n collect (intern (format nil "$~d" i)))))
+    (loop with groupings = (list ())
+       for ((symbol . expansion) . body) in productions 
+       for arg-list = (make-arg-list (length expansion))
+       for yacc-production = `((,@expansion #'(lambda ,arg-list
+						(declare (ignorable ,@arg-list))
+						,@body)))
+       for previous-prod = (find symbol groupings :key #'first)
+       do (if previous-prod
+	      (setf (cdr (last previous-prod)) yacc-production)
+	      (setf (cdr (last groupings)) (list (cons symbol yacc-production))))
+       finally (pprint (rest groupings)) (return (rest groupings)))))
+
+(defmacro yacc-parser ((name &rest args) &rest productions)
+  `(yacc:define-parser ,name
+       ,@args 
+       ,@(convert-parser-to-cl-yacc productions))))
+
+#+use-cl-yacc
+(yacc-parser (*pdf-parser*
+	      (:start-symbol pdf-data)
+	      (:terminals (:xref :trailer :n :f :obj :endobj :integer :token :start-dict :end-dict :name
+				 :string :true :false :start-array :end-array :r :endstream :stream)))
+  ((pdf-data trailer) $1)
+  ((pdf-data object) $1)
+  ((trailer :xref xrefs :trailer dictionary) (setf *force-eof* t)(list $2 $4))
+  ((xrefs xref xrefs) (cons $1 $2))
+  ((xrefs xref) (list $1))
+  ((xref integer integer xref-type) (list $1 $2 $3))
+  ((xref-type ) :start-table)
+  ((xref-type :n) :n)
+  ((xref-type :f) :f)
+  ((object number gen-number :obj content :endobj) (setf *force-eof* t)(list $1 $2 $4))
+  ((number integer) $1)
+  ((gen-number integer) $1)
+  ((content val) $1)
+  ((integer :integer) $1)
+  ((token :token) $1)
+  ((dictionary :start-dict key-val* :end-dict) (%make-dictionary $2))
+  ((dictionary :start-dict :end-dict) (%make-dictionary nil))
+  ((key-val* key-val key-val*) (cons $1 $2))
+  ((key-val* key-val) (list $1))
+  ((key-val key val) (cons $1 $2))
+  ((key-val key object-ref) (cons $1 $2))
+  ((key token) $1)
+  ((val token) $1)
+  ((val integer) $1)
+  ((val :name) $1)
+  ((val :string) $1)
+  ((val :true) :true)
+  ((val :false) :false)
+  ((val stream) $1)
+  ((val dictionary) $1)
+  ((val array) $1)
+  ((array :start-array values :end-array) (convert-to-array $2))
+  ((array :start-array :end-array) (vector))
+  ((values val values) (cons $1 $2))
+  ((values :r values) (cons :r $2))
+  ((values val) (list $1))
+  ((values :r) (list :r))
+  ((stream  start-stream :endstream) $1)
+  ((start-stream :start-dict key-val* :end-dict :stream)(%make-stream $2))
+  ((object-ref integer integer :R) (%make-obj-ref $1 $2)))
+
+#-use-cl-yacc
 (parsergen:defparser pdf-object-parser
   ((Start pdf-data) $1)
   ((pdf-data trailer) $1)
   ((pdf-data object) $1)
-  ((trailer :xref first last xrefs :trailer dictionary) (setf *force-eof* t)(list $2 $3 $4 $6))
-  ((first integer) $1)
-  ((last integer) $1)
+  ((trailer :xref xrefs :trailer dictionary) (setf *force-eof* t)(list $2 $4))
   ((xrefs xref xrefs) (cons $1 $2))
   ((xrefs xref) (list $1))
   ((xref integer integer xref-type) (list $1 $2 $3))
+  ((xref-type ) :start-table)
   ((xref-type :n) :n)
   ((xref-type :f) :f)
   ((object number gen-number :obj content :endobj) (setf *force-eof* t)(list $1 $2 $4))
@@ -181,14 +268,14 @@
   ((val dictionary) $1)
   ((val array) $1)
   ((array :start-array values :end-array) (convert-to-array $2))
+  ((array :start-array :end-array) (vector))
   ((values val values) (cons $1 $2))
   ((values :r values) (cons :r $2))
   ((values val) (list $1))
   ((values :r) (list :r))
   ((stream  start-stream :endstream) $1)
   ((start-stream :start-dict key-val* :end-dict :stream)(%make-stream $2))
-  ((object-ref integer integer :R) (%make-obj-ref $1 $2))
-  )
+  ((object-ref integer integer :R) (%make-obj-ref $1 $2)))
 
 (defun %make-stream (key-vals)
   (let ((dict (make-instance 'pdf-stream :empty t)))
@@ -214,7 +301,8 @@
 (defun parse-object (position)
   (file-position *pdf-read-stream* position)
   (let* ((*force-eof* nil)
-	 (data (pdf-object-parser 'pdf-lex))
+	 (data #-use-cl-yacc (pdf-object-parser 'pdf-lex)
+	       #+use-cl-yacc (yacc:parse-with-lexer 'pdf-lex *pdf-parser*))
 	 (object (aref (objects *document*) (first data)))
 	 (content (third data))
 	 dict)
@@ -228,13 +316,30 @@
       (setf raw-obj (aref (objects *document*) obj-num)))
     (with-input-from-string (*pdf-read-stream* (content raw-obj))
       (let* ((*force-eof* nil)
-	     (data (pdf-object-parser 'pdf-lex))
+	     (data #-use-cl-yacc (pdf-object-parser 'pdf-lex)
+		   #+use-cl-yacc (yacc:parse-with-lexer 'pdf-lex *pdf-parser*))
 	     (new-obj (make-instance 'indirect-object :no-link t :obj-number obj-num
 				     :gen-number (gen-number raw-obj) :content (third data))))
 	(setf (aref (objects *document*) obj-num) new-obj)
 	new-obj))))
 
-(defun open-page (page-num before)
+(defvar *original-content* nil)
+(defvar *current-content* nil)
+
+(defun insert-original-page-content ()
+  (write-line " q" *page-stream*)
+  (vector-push-extend (make-instance 'indirect-object :content 
+				     (make-instance 'pdf-stream :content 
+						    (get-output-stream-string *page-stream*)))
+		      *current-content*)
+  (if (vectorp *original-content*)
+      (loop for content across *original-content* do
+	   (vector-push-extend content *current-content*))
+      (vector-push-extend *original-content* *current-content*))
+  (setf *page-stream* (make-string-output-stream))
+  (write-line " Q" *page-stream*))
+
+(defun open-page (page-num)
   (let* ((page-obj-num (obj-number (aref (pages *root-page*) page-num)))
 	 (page (parse-raw-object (aref (objects *document*) page-obj-num)))
 	 (dict (content page))
@@ -244,8 +349,9 @@
 	(setf resources (content (parse-raw-object resources)))))
     (let ((fonts (get-dict-value resources "/Font"))
 	  (xobjects (get-dict-value resources "/Font"))
-	  (content-stream (make-instance 'pdf-stream))
-	  (original-content (get-dict-value dict "/Contents")))
+	  (content-stream (make-instance 'pdf-stream)))
+      (setf *original-content* (get-dict-value dict "/Contents"))
+      (setf *current-content* (make-array 10 :fill-pointer 0 :adjustable t ))
       (change-class page 'page)
       (unless fonts
 	(setf fonts (make-instance 'dictionary))
@@ -258,13 +364,8 @@
 	    (font-objects page) fonts
 	    (xobjects page) xobjects
 	    (content-stream page) content-stream)
-      (change-dict-value dict "/Contents"
-			 (if (vectorp original-content)
-			   (concatenate 'vector original-content 
-					(vector (make-instance 'indirect-object :content content-stream)))
-			   (vector original-content
-				   (make-instance 'indirect-object :content content-stream))))
-      page)))
+      (change-dict-value dict "/Contents" *current-content*))
+    page))
 
 (defun read-raw-object (position number gen sorted-positions)
   (file-position *pdf-read-stream* position)
@@ -275,71 +376,84 @@
     (make-instance 'raw-object :content content :no-link t
 		   :obj-number number :gen-number gen)))
 
-(defmacro with-existing-document ((file) &body body)
+(defmacro with-existing-document ((file &key (creator "") author title subject keywords) &body body)
   `(let* ((*document* (parse-pdf ,file))
-	  (*root-page* (root-page *document*)))
+	  (*root-page* (root-page *document*))
+	  (*page-number* 0))
+     (add-doc-info *document* :creator ,creator :author ,author
+		   :title ,title :subject ,subject :keywords ,keywords)
     ,@body))
+
 (export 'with-existing-document)
 
-(defmacro with-existing-page ((page-number before) &body body)
-  `(let* ((*page* (open-page ,page-number ,before)))
-    (setf (content (content-stream *page*))
-     (with-output-to-string (*page-stream*)
-			    ,@body))))
+(defmacro with-existing-page ((page-number) &body body)
+  `(let* ((*original-content* nil)
+	  (*current-content* nil)
+	  (*page* (open-page ,page-number)))
+     (setf (content (content-stream *page*))
+	   (let ((*page-stream* (make-string-output-stream)))
+	     (declare (dynamic-extent pdf::*page-stream*))
+	     ,@body
+	     (vector-push-extend
+	      (make-instance 'indirect-object :content 
+			     (make-instance 'pdf-stream :content 
+					    (get-output-stream-string pdf::*page-stream*)))
+	      *current-content*)))))
+
 (export 'with-existing-page)
 
 
 #|
 
-(pdf:with-existing-document (#P"/tmp/2042.pdf")
-    (pdf:with-page ()
-      (let ((helvetica (make-instance 'pdf:font)))
-	(pdf:add-fonts-to-page helvetica)
-	(pdf:in-text-mode
-	 (pdf:set-font helvetica 36.0)
-	 (pdf:move-text 100 800)
-	 (pdf:draw-text "cl-pdf: Example 1"))
-	(pdf:translate 230 500)
-	(loop repeat 101
-	      for i = 0.67 then (* i 1.045)
-	      do (pdf:in-text-mode
-		  (pdf:set-font helvetica i)
-		  (pdf:move-text (* i 3) 0)
-		  (pdf:draw-text "rotation"))
-	      (pdf:rotate 18))))
-    (pdf:write-document #P"/t.pdf"))
-
-(pdf:with-existing-document (#P"/tmp/2042.pdf")
-    (pdf:with-existing-page (0 nil)
-      (let ((helvetica (make-instance 'pdf:font)))
-	(pdf:add-fonts-to-page helvetica)
-	(pdf:in-text-mode
-	 (pdf:set-font helvetica 36.0)
-	 (pdf:move-text 100 800)
-	 (pdf:draw-text "cl-pdf: Example 1"))
-	(pdf:translate 230 500)
-	(loop repeat 101
-	      for i = 0.67 then (* i 1.045)
-	      do (pdf:in-text-mode
-		  (pdf:set-font helvetica i)
-		  (pdf:move-text (* i 3) 0)
-		  (pdf:draw-text "rotation"))
-	      (pdf:rotate 18))))
-    (pdf:with-page ()
-      (let ((helvetica (make-instance 'pdf:font)))
-	(pdf:add-fonts-to-page helvetica)
-	(pdf:in-text-mode
-	 (pdf:set-font helvetica 36.0)
-	 (pdf:move-text 100 800)
-	 (pdf:draw-text "cl-pdf: Example 1"))
-	(pdf:translate 230 500)
-	(loop repeat 101
-	      for i = 0.67 then (* i 1.045)
-	      do (pdf:in-text-mode
-		  (pdf:set-font helvetica i)
-		  (pdf:move-text (* i 3) 0)
-		  (pdf:draw-text "rotation"))
-	      (pdf:rotate 18))))
-    (pdf:write-document #P"/t.pdf"))
+(pdf:with-existing-document (#P"/tmp/MS-32.pdf")
+  (pdf:with-existing-page (0)
+    (let ((helvetica (pdf:get-font "Helvetica")))
+      (pdf:in-text-mode
+       (pdf:set-font helvetica 45.0)
+       (pdf:move-text 100 700)
+       (pdf:draw-text "cl-pdf-parser example"))
+      (pdf:with-saved-state
+	  (pdf:translate 100 100)
+	(pdf:scale 0.7 0.7)
+	(pdf:rotate 0)
+	(insert-original-page-content)
+	(pdf:translate 100 100)
+	(pdf:scale 0.5 0.5)
+	(pdf:rotate 30)
+	(insert-original-page-content))
+      (pdf:translate 230 400)
+      (loop repeat 170
+	 for i = 0.67 then (* i 1.03)
+	 do (pdf:in-text-mode
+	     (pdf:set-font helvetica i)
+	     (pdf:set-rgb-fill (/ (random 255) 255.0)(/ (random 255) 255.0)(/ (random 255) 255.0))
+	     (pdf:move-text (* i 3) 0)
+	     (pdf:draw-text "cl-pdf"))
+	   (pdf:rotate 13))))
+  (pdf:with-existing-page (1)
+    (let ((helvetica (pdf:get-font "Helvetica")))
+      (insert-original-page-content)
+      (pdf:in-text-mode
+       (pdf:set-font helvetica 60.0)
+       (pdf:move-text 250 250)
+       (pdf:rotate 30)
+       (pdf:set-rgb-fill 1.0 0.0 0.0)
+       (pdf:draw-text "cl-pdf-parser example"))))
+  (pdf:with-page ()       ;add a new page
+    (let ((helvetica (pdf:get-font "Helvetica")))
+      (pdf:in-text-mode
+       (pdf:set-font helvetica 36.0)
+       (pdf:move-text 100 800)
+       (pdf:draw-text "cl-pdf: Example 1"))
+      (pdf:translate 230 500)
+      (loop repeat 150
+	 for i = 0.67 then (* i 1.045)
+	 do (pdf:in-text-mode
+	     (pdf:set-font helvetica i)
+	     (pdf:set-rgb-fill (/ (random 255) 255.0)(/ (random 255) 255.0)(/ (random 255) 255.0))
+	     (pdf:move-text (* i 3) 0)
+	     (pdf:draw-text "cl-typesetting"))
+	   (pdf:rotate 13))))
+  (pdf:write-document #P"/tmp/t.pdf"))
 
 |#
