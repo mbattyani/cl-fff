6db002f9405b855131c0a40b355b686d0c2dc3e8
mbattyani <marc@battyani.com>
2014-03-22 22:59:24 -0400

    whoops according to http://jsperf.com/getelementbyid-vs-jquery-id/44 it looks like the jQuery $ selector is more than 14 times slower than getElementById... Going back to the 15 years old version!

--- meta-web/web-resources/static/fractal.js
+++ meta-web/web-resources/static/fractal.js
@@ -1,6 +1,11 @@
 function fgt(name)
 {
-  return $('#'+name)[0];
+  var item;
+  if (document.getElementById)
+    item=document.getElementById(name);
+  else
+    item=document.all[name];
+  return item;
 }
 
 function f85425(Tabs, Panes, nTab, BaseClassName, SelectedClassName, remove)
